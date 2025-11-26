fe0d65d1f219e0679352607cf652270de50dc412
Adria <blladria@hotmail.com>
2025-11-26 15:13:29 +0100

    CODIGO:  quitado carácteres sospechosos

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+# -*- coding: utf-8 -*-
 from mcpi.minecraft import Minecraft
 
 def test_connection():
     # Intenta conectarse al servidor (debe estar iniciado en localhost)
     try:
         mc = Minecraft.create()
-        mc.postToChat("¡Sistema TAP iniciado y conectado!")
-        print("Conexión exitosa. Mensaje enviado al chat.")
+        mc.postToChat("Sistema TAP iniciado y conectado")
+        print("Conexion exitosa. Mensaje enviado al chat.")
     except Exception as e:
         print(f"Error al conectar con Minecraft: {e}")
-        print("Asegúrate de que el servidor de Minecraft esté corriendo.")
+        print("Asegurate de que el servidor de Minecraft este corriendo.")
 
 if __name__ == "__main__":
     test_connection()
